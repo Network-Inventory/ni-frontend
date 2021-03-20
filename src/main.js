31d0e1da1065e1211eb643039f72fa1b3b26a980
Andreas Zweili <andreas@2li.ch>
2021-03-20 15:16:07 +0100

    add an axios baseURL
    This is currently only for testing.
    Later the URL should be read from an .env file

--- src/main.js
+++ src/main.js
@@ -4,5 +4,9 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
+import axios from "axios"
+
+axios.defaults.baseURL = "http://localhost:8000/api/"
+
 const app = createApp(App);
 app.mount("#app");
