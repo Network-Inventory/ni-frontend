69e9f3042f45e8e1eea34fa6add6e48dbf96e29d
Andreas Zweili <andreas@2li.ch>
2021-03-22 13:33:55 +0100

    remove duplicate code
    this is already defined in axios-api.js

--- src/main.js
+++ src/main.js
@@ -4,9 +4,5 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
-import axios from "axios";
-
-axios.defaults.baseURL = "http://localhost:8000/api/";
-
 const app = createApp(App);
 app.mount("#app");
