f2f3f6d8591d42e3b764135c09b1d144ab64b7d6
Andreas Zweili <andreas@2li.ch>
2021-03-22 13:27:22 +0100

    linting

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 
-import axios from "axios"
+import axios from "axios";
 
-axios.defaults.baseURL = "http://localhost:8000/api/"
+axios.defaults.baseURL = "http://localhost:8000/api/";
 
 const app = createApp(App);
 app.mount("#app");
