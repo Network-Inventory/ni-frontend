76510c88edf73fbc9e4aeb63535229339265ac5f
Andreas Zweili <andreas@2li.ch>
2021-03-22 17:56:06 +0100

    add a check to return the correct URL for the API

--- src/components/scripts/axios-api.js
+++ src/components/scripts/axios-api.js
@@ -1,8 +1,20 @@
 import axios from "axios";
 import Config from "../../config.json";
 
+function getBaseUrl() {
+  if (Config.NI_BASE_URL === "$NI_BASE_URL") {
+    return "http://" + process.env.NI_BASE_URL;
+  } else if (Config.NI_MODE === "development") {
+    return "http://" + Config.NI_BASE_URL;
+  } else {
+    return "https://" + Config.NI_BASE_URL;
+  }
+}
+
+const apiURL = getBaseUrl();
+
 const getAPI = axios.create({
-  baseURL: Config.NI_BASE_URL,
+  baseURL: apiURL,
   timeout: 1000,
 });
 
