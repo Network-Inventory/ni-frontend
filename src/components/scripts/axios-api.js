6aa8c482dc3ef3454cea2d5ad1f06453ab91dd63
Andreas Zweili <andreas@2li.ch>
2021-03-24 22:51:28 +0100

    rewrite the getAPI component

--- src/components/scripts/axios-api.js
+++ src/components/scripts/axios-api.js
@@ -1,24 +1,7 @@
 import axios from "axios";
-import Config from "../../config.json";
-
-function getBaseUrl() {
-  if (process.env.NODE_ENV === "development") {
-    return "http://localhost/api/";
-  } else if (Config.NI_MODE === "development") {
-    console.log(Config.NI_HOST);
-    return "http://" + Config.NI_HOST + "/api/";
-  } else {
-    console.log(Config.NI_HOST);
-    return "https://" + Config.NI_HOST + "/api/";
-  }
-}
-
-const apiURL = getBaseUrl();
-
-console.log("Connected to API at: " + apiURL);
 
 const getAPI = axios.create({
-  baseURL: apiURL,
+  baseURL: location.protocol + "//" + location.hostname + "/api/",
   timeout: 1000,
 });
 
