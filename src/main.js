5805f58a596e41eb3032183233751e750cc1a5b0
Andreas Zweili <andreas@2li.ch>
2021-03-27 13:24:44 +0100

    move app.mount to the correct position

--- src/main.js
+++ src/main.js
@@ -6,8 +6,8 @@ import App from "./App.vue";
 import BaseButton from "./components/UI/BaseButton";
 
 const app = createApp(App);
-app.mount("#app");
 app.component("base-button", BaseButton);
+app.mount("#app");
 
 if (module.hot) {
   module.hot.accept();
