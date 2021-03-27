dd9ab9321d81200f9b5823061c365a00319714b7
Andreas Zweili <andreas@2li.ch>
2021-03-27 21:23:18 +0100

    add a navigation bar

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,18 @@
 <template>
-  <router-view></router-view>
-  <the-footer></the-footer>
+  <the-navigation></the-navigation>
+  <main>
+    <router-view></router-view>
+    <the-footer></the-footer>
+  </main>
 </template>
 
 <script>
+import TheNavigation from "./components/nav/TheNavigation.vue";
 import TheFooter from "./components/layout/TheFooter.vue";
 
 export default {
   components: {
+    TheNavigation,
     TheFooter,
   },
   data() {
@@ -20,12 +25,16 @@ export default {
 * {
   box-sizing: border-box;
 }
+
 html {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+body {
+  margin: 0;
 }
 </style>
