462390d45788ddb0cc25e6244cbd7e1176fbc96f
Andreas Zweili <andreas@2li.ch>
2021-03-27 15:41:53 +0100

    move the card CSS to the BaseCard

--- src/components/UI/BaseCard.vue
+++ src/components/UI/BaseCard.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card">
+  <div>
     <slot name="default"></slot>
   </div>
 </template>
@@ -8,4 +8,13 @@
 export default {};
 </script>
 
-<style></style>
+<style scoped>
+div {
+  margin: 2rem auto;
+  max-width: 40rem;
+  border-radius: 12px;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  padding: 2rem;
+  background-color: #ffffff;
+}
+</style>
