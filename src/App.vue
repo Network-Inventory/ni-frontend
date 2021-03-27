7a59f1dbee4cb70c88aac31b6352be4593fd4862
Andreas Zweili <andreas@2li.ch>
2021-03-27 15:41:53 +0100

    move the card CSS to the BaseCard

--- src/App.vue
+++ src/App.vue
@@ -30,12 +30,4 @@ html {
   color: #2c3e50;
   margin-top: 60px;
 }
-.card {
-  margin: 2rem auto;
-  max-width: 40rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 2rem;
-  background-color: #ffffff;
-}
 </style>
