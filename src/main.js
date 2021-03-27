d2c190bc8d7731d6a2006cc05c27ae884f644649
Andreas Zweili <andreas@2li.ch>
2021-03-27 13:25:02 +0100

    add base-dialog

--- src/main.js
+++ src/main.js
@@ -4,9 +4,11 @@ import { createApp } from "vue";
 
 import App from "./App.vue";
 import BaseButton from "./components/UI/BaseButton";
+import BaseDialog from "./components/UI/BaseDialog";
 
 const app = createApp(App);
 app.component("base-button", BaseButton);
+app.component("base-dialog", BaseDialog);
 app.mount("#app");
 
 if (module.hot) {
