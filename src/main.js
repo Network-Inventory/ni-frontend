cfe9aa1386407520b38ea9eeae522d14521d27d2
Andreas Zweili <andreas@2li.ch>
2021-05-04 16:43:16 +0200

    move the router into its own file

--- src/main.js
+++ src/main.js
@@ -1,46 +1,15 @@
 "use strict";
 
 import { createApp } from "vue";
-import { createRouter, createWebHistory } from "vue-router";
 
 import App from "./App.vue";
+import router from "./router.js";
+
 import BaseButton from "./components/UI/BaseButton";
 import BaseCard from "./components/UI/BaseCard";
 import BaseDialog from "./components/UI/BaseDialog";
 import BaseRouterButton from "./components/UI/BaseRouterButton";
 import BaseTable from "./components/UI/BaseTable";
-import TheCustomers from "./components/customers/TheCustomers";
-import CustomerDetails from "./components/customers/CustomerDetails";
-import ComputerDetails from "./components/computers/ComputerDetails";
-import TheComputers from "./components/computers/TheComputers";
-
-const router = createRouter({
-  history: createWebHistory(),
-  routes: [
-    { name: "customers", path: "/", component: TheCustomers },
-    {
-      name: "customer-details",
-      path: "/customers/:customerId",
-      component: CustomerDetails,
-      props: true,
-    },
-    { name: "computers", path: "/computers", component: TheComputers },
-    {
-      name: "computer-details",
-      path: "/computers/:computerId",
-      component: ComputerDetails,
-      props: true,
-    },
-    { path: "/:notFound(.*)", component: TheCustomers },
-  ],
-  linkActiveClass: "active",
-  srollBehaviour(_, _2, savedPosition) {
-    if (savedPosition) {
-      return savedPosition;
-    }
-    return { left: 0, top: 0 };
-  },
-});
 
 const app = createApp(App);
 app.component("base-card", BaseCard);
---
+++ src/router.js
@@ -0,0 +1,38 @@
+"use strict";
+
+import { createRouter, createWebHistory } from "vue-router";
+
+import TheCustomers from "./components/customers/TheCustomers";
+import CustomerDetails from "./components/customers/CustomerDetails";
+import ComputerDetails from "./components/computers/ComputerDetails";
+import TheComputers from "./components/computers/TheComputers";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    { name: "customers", path: "/", component: TheCustomers },
+    {
+      name: "customer-details",
+      path: "/customers/:customerId",
+      component: CustomerDetails,
+      props: true,
+    },
+    { name: "computers", path: "/computers", component: TheComputers },
+    {
+      name: "computer-details",
+      path: "/computers/:computerId",
+      component: ComputerDetails,
+      props: true,
+    },
+    { path: "/:notFound(.*)", component: TheCustomers },
+  ],
+  linkActiveClass: "active",
+  srollBehaviour(_, _2, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
+    return { left: 0, top: 0 };
+  },
+});
+
+export default router;
