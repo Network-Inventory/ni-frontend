7c92e57ecacdbcda94e0e774e29dff22a15152ca
Andreas Zweili <andreas@zweili.ch>
2021-09-15 20:43:57 +0200

    port the router

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,19 +1,35 @@
+const TheCustomers = () => import("../components/customers/TheCustomers");
+const CustomerDetails = () => import("../components/customers/CustomerDetails");
+const ComputerDetails = () => import("../components/computers/ComputerDetails");
+const TheComputers = () => import("../components/computers/TheComputers");
 
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/Index.vue') }
-    ]
+    path: "/",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/Index.vue") }],
   },
-
+  { name: "customers", path: "/customers", component: TheCustomers },
+  {
+    name: "customer-details",
+    path: "/customers/:customerId",
+    component: CustomerDetails,
+    props: true,
+  },
+  { name: "computers", path: "/computers", component: TheComputers },
+  {
+    name: "computer-details",
+    path: "/computers/:computerId",
+    component: ComputerDetails,
+    props: true,
+  },
+  { path: "/:notFound(.*)", component: TheCustomers },
   // Always leave this as last one,
   // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/Error404.vue')
-  }
-]
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/Error404.vue"),
+  },
+];
 
-export default routes
+export default routes;
