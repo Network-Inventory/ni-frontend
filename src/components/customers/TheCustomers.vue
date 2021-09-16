2bfcf4f68465f3ff5abda07b58346d34191b9a55
Andreas Zweili <andreas@zweili.ch>
2021-09-16 23:13:50 +0200

    replace the customers table

--- src/components/customers/TheCustomers.vue
+++ src/components/customers/TheCustomers.vue
@@ -5,52 +5,94 @@
     @cancel="closeDialog"
   ></add-customer-dialog>
 
-  <base-card>
-    <header><h1>Customers</h1></header>
-    <base-button @click="openAddCustomer()">Add Customer</base-button>
+  <div class="q-pa-md" v-if="allGood">
     <div v-if="isLoading">
       <base-spinner></base-spinner>
     </div>
-    <base-table v-if="allGood">
-      <tr>
-        <th class="orderable">Name</th>
-        <th>Nets</th>
-        <th>Computers</th>
-        <th>Devices</th>
-        <th>Backups</th>
-        <th>Licenses</th>
-        <th>Users</th>
-        <th>Actions</th>
-      </tr>
+    <div class="q-pa-md q-gutter-sm">
+      <q-btn color="primary" label="Add Customer" @click="openAddCustomer()" />
+    </div>
 
-      <tr v-for="customer in data.response" :key="customer.url">
-        <td>
-          <router-link
-            :to="{
-              name: 'customer-details',
-              params: { customerId: getId(customer.url) },
-            }"
-            >{{ customer.name }}</router-link
-          >
-        </td>
-        <td><a :href="customer.url">Nets</a></td>
-        <td><a :href="customer.url">Computers</a></td>
-        <td><a :href="customer.url">Devices</a></td>
-        <td><a :href="customer.url">Backups</a></td>
-        <td><a :href="customer.url">Licenses</a></td>
-        <td><a :href="customer.url">Users</a></td>
-        <td>
-          <a href="#" @click="deleteCustomer(customer.url)">delete</a>
-        </td>
-      </tr>
-    </base-table>
+    <q-table
+      @row-click="openDetails"
+      title="Customers"
+      :rows="data.response"
+      :columns="columns"
+      row-key="name"
+    >
+      <template v-slot:body-cell-actions="props">
+        <q-td :props="props">
+          <div>
+            <q-btn
+              flat
+              round
+              color="red"
+              icon="delete"
+              @click.stop="deleteCustomer(props.row.url)"
+            />
+          </div>
+        </q-td>
+      </template>
+    </q-table>
+  </div>
 
-    <p v-else>Couldn't fetch the computer details.</p>
-  </base-card>
+  <p v-else>Couldn't fetch the computer details.</p>
 </template>
 
 <script>
+const columns = [
+  {
+    name: "name",
+    required: true,
+    label: "Name",
+    align: "left",
+    field: "name",
+    sortable: true
+  },
+  {
+    name: "nets",
+    label: "Nets",
+    field: "nets",
+    sortable: true
+  },
+  {
+    name: "computers",
+    label: "Computers",
+    field: "computers",
+    sortable: true
+  },
+  {
+    name: "devices",
+    label: "Devices",
+    field: "devices",
+    sortable: true
+  },
+  {
+    name: "backups",
+    label: "Backups",
+    field: "backups",
+    sortable: true
+  },
+  {
+    name: "licenses",
+    label: "Licenses",
+    field: "licenses",
+    sortable: true
+  },
+  {
+    name: "users",
+    label: "Users",
+    field: "users",
+    sortable: true
+  },
+  {
+    name: "actions",
+    label: "Actions",
+    sortable: true
+  }
+];
 import { ref } from "vue";
+import { useRouter } from "vue-router";
 import getAPI from "../../scripts/axios-api";
 import { useGetObjects } from "../../hooks/GetData";
 import getId from "../../scripts/get-id-from-url";
@@ -58,20 +100,28 @@ import AddCustomerDialog from "./AddCustomer.vue";
 
 export default {
   components: {
-    AddCustomerDialog,
+    AddCustomerDialog
   },
   setup() {
     const addCustomerDialogVisible = ref(false);
 
+    const router = useRouter();
     const { isLoading, data, allGood, getData } = useGetObjects("/customers");
 
+    function openDetails(_, row) {
+      const id = getId(row.url);
+      router.push({
+        name: "customer-details",
+        params: { customerId: id }
+      });
+    }
     function deleteCustomer(url) {
       getAPI
         .delete(url)
         .then(() => {
           getData();
         })
-        .catch((err) => {
+        .catch(err => {
           console.log(err);
         });
     }
@@ -98,7 +148,9 @@ export default {
       getId,
       allGood,
       data,
+      openDetails,
+      columns
     };
-  },
+  }
 };
 </script>
