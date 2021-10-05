1f81f3c9eaeabb9cb0b717342f06a7f32021711b
Andreas Zweili <andreas@zweili.ch>
2021-10-05 17:20:14 +0200

    Add customer details

--- src/components/customers/CustomerDetails.vue
+++ src/components/customers/CustomerDetails.vue
@@ -2,9 +2,9 @@
   <div class="q-pa-md">
     <q-card>
       <q-card-section>
-        <div class="text-h6">{{ data.name }}</div>
+        <div class="text-h6">{{ data.customer?.name }}</div>
       </q-card-section>
-      <q-card-section>{{ data.description }}</q-card-section>
+      <q-card-section>{{ data.customer?.description }}</q-card-section>
       <q-card-actions>
         <q-btn label="Back" color="primary" @click="$router.go(-1)" />
       </q-card-actions>
@@ -13,12 +13,18 @@
 </template>
 
 <script>
+import { reactive } from "vue";
 import { useStore } from "vuex";
 export default {
   props: ["customerId"],
   setup(props) {
     const $store = useStore();
-    const data = $store.dispatch("customers/customerDetails", props.id);
+    const data = reactive({ customer: {} });
+    $store
+      .dispatch("customers/customerDetails", props.customerId)
+      .then(function (customer) {
+        data.customer = customer;
+      });
 
     return {
       data,
