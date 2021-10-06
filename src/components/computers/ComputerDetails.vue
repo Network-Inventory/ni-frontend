2d751790e8a18d36ed414d8b9f460deabcdc1429
Andreas Zweili <andreas@zweili.ch>
2021-10-06 17:30:16 +0200

    add more details

--- src/components/computers/ComputerDetails.vue
+++ src/components/computers/ComputerDetails.vue
@@ -4,7 +4,89 @@
       <q-card-section>
         <div class="text-h6">{{ data?.name }}</div>
       </q-card-section>
-      <q-card-section>{{ data?.description }}</q-card-section>
+
+      <div class="q-pa-md">
+        <q-list>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Description</q-item-label>
+              <q-item-label>{{ data?.description }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Serialnumber</q-item-label>
+              <q-item-label>{{ data?.serialnumber }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Installation Date</q-item-label>
+              <q-item-label>{{ data?.installation_date }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Category</q-item-label>
+              <q-item-label>{{ data?.category.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Owner</q-item-label>
+              <q-item-label>{{ data?.owner.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Customer</q-item-label>
+              <q-item-label>{{ data?.customer.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Manufacturer</q-item-label>
+              <q-item-label>{{ data?.manufacturer.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Model</q-item-label>
+              <q-item-label>{{ data?.model.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Location</q-item-label>
+              <q-item-label>{{ data?.location.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>User</q-item-label>
+              <q-item-label>{{ data?.user.name }}</q-item-label>
+            </q-item-section>
+          </q-item>
+          <q-item>
+            <q-item-section>
+              <q-item-label overline>Nets</q-item-label>
+              <div v-if="data?.net">
+                <q-item
+                  v-for="net in data?.net"
+                  :key="net.id"
+                  :to="{
+                    name: 'computer-details',
+                    params: { computerId: net.url },
+                  }"
+                  >{{ net.name }}</q-item
+                >
+              </div>
+            </q-item-section>
+          </q-item>
+        </q-list>
+      </div>
+
       <q-card-actions>
         <q-btn label="Back" color="primary" @click="$router.go(-1)" />
       </q-card-actions>
@@ -15,6 +97,8 @@
 <script>
 import { ref } from "vue";
 import { useStore } from "vuex";
+import getId from "../../scripts/get-id-from-url";
+
 export default {
   props: ["computerId"],
   setup(props) {
@@ -29,6 +113,7 @@ export default {
 
     return {
       data,
+      getId,
     };
   },
 };
