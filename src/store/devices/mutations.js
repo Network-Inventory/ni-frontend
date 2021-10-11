89a0a45e78a0be6a7e05a86f3a01b122fd436373
Andreas Zweili <andreas@zweili.ch>
2021-10-11 18:44:27 +0200

    fix another typo

--- src/store/devices/mutations.js
+++ src/store/devices/mutations.js
@@ -4,7 +4,7 @@ export function setDevices(state, payload) {
 export function addNewDevice(state, payload) {
   state.devices.push(payload);
 }
-export function setDeviceManufaturers(state, payload) {
+export function setDeviceManufacturers(state, payload) {
   state.device_manfucaturers = payload;
 }
 export function addNewDeviceManufacturer(state, payload) {
