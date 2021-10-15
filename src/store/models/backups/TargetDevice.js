608a9b8160d844b96dcbf7fd25601dd511e31854
Andreas Zweili <andreas@zweili.ch>
2021-10-15 19:09:58 +0200

    finish TargetDevice

--- src/store/models/backups/TargetDevice.js
+++ src/store/models/backups/TargetDevice.js
@@ -1,5 +1,8 @@
 import { Model } from "@vuex-orm/core";
 
+import { Computer } from "../computers";
+import { Backup } from ".";
+
 export default class TargetDevice extends Model {
   static entity = "target-devices";
 
@@ -7,6 +10,12 @@ export default class TargetDevice extends Model {
     return {
       id: this.number(null),
       name: this.attr(""),
+      device_id: this.number(null),
+      backup_id: this.number(null),
+      target_path: this.attr(""),
+      // relationships
+      device: this.belongsTo(Computer, "device_id"),
+      backup: this.belongsTo(Backup, "backup_id"),
     };
   }
   static fetch() {
