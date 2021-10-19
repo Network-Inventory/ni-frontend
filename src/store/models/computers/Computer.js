d52a14b2359624d3681dd01306112229e9d5c3db
Andreas Zweili <andreas@zweili.ch>
2021-10-19 21:30:33 +0200

    extend the Computer model

--- src/store/models/computers/Computer.js
+++ src/store/models/computers/Computer.js
@@ -1,17 +1,38 @@
-import { Model } from "@vuex-orm/core";
+//import { Model } from "@vuex-orm/core";
 
-export default class Computer extends Model {
+import { Device } from "../devices";
+
+export default class Computer extends Device {
   static entity = "computers";
-
+  //static baseEntity = "devices";
   static fields() {
     return {
-      id: this.number(null),
-      name: this.attr(""),
+      ...super.fields(),
+      os: this.number(null),
+      allocated_space: this.number(null),
+      host_id: this.number(null),
+      cpu_ids: this.attr([]),
+      ram_ids: this.attr([]),
+      gpu_ids: this.attr([]),
+      disk_ids: this.attr([]),
+      software_ids: this.attr([]),
     };
   }
   static fetch() {
     return this.api().get("computers", {
-      dataKey: "results",
+      // dataKey: "results",
+      dataTransformer: ({ data }) => {
+        const result = [];
+        data["results"].forEach((computer) => {
+          computer.cpu_ids = computer.cpu;
+          computer.ram_ids = computer.ram;
+          computer.gpu_ids = computer.gpu;
+          computer.disk_ids = computer.disks;
+          computer.software_ids = computer.software;
+          result.push(computer);
+        });
+        return result;
+      },
     });
   }
   static getById(id) {
